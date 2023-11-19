d80c14b326b2730e9165aced7f46f8b4459ebc7a
Alfin89 <rebahansantuy30@gmail.com>
2023-11-19 22:13:54 +0700

    add learning ejs and express js

--- two-projects/index.js
+++ two-projects/index.js
@@ -2,14 +2,34 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const Bigdata = require('./data.json');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
+app.use(express.static(path.join(__dirname, '/public')));
 
 
 // Route
 app.get('/', (req, res) => {
-    res.render('home')
+    const wmk = [
+        'Alfin kamil', 'Habiburrohman', 'Rohman', 'Ulfa', 'Waid',
+    ]
+    res.render('home', { wmk })
+})
+
+app.get('/random', (req, res) => {
+    const number = Math.floor(Math.random() * 100 ) + 1;
+    res.render('random', { number })
+})
+
+app.get('/parsing/:id', (req, res) => {
+    const { id } = req.params;
+    const data = Bigdata[id];
+    if (data) {
+        res.render('parsing', { data })
+    } else {
+        res.render('notfound', { id })
+    }
 })
 
 // app.listen(3000, () => {
